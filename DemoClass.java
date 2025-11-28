399542fc85a2e5b7a3948f87644f382255359c8b
dharmishthakothari <dharmishtha.only@gmail.com>
2025-11-28 13:18:41 +0530

    Add files via upload

---
+++ DemoClass.java
@@ -0,0 +1,21 @@
+package pkgFirst;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World...Avani");
+		System.out.println("Hello Dhruvi");
+		
+		int no1=20,no2=550;
+		System.out.println(no1);
+		System.out.println(no2);
+		
+		System.out.println("Number1 = "+no1+" Number2 = "+no2);
+		
+		
+		
+		
+	}
+
+}
---
+++ PersonData.java
@@ -0,0 +1,18 @@
+package pkgFirst;
+
+public class PersonData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age=20;
+		long c_no=9090909090L;
+		float percentage=89.56F;
+		String name="Dhruvi";
+		System.out.println(name+age+c_no+percentage);
+		System.out.println(name+"\t"+age+"\t"+c_no+"\t"+percentage);
+		System.out.println("Name = "+name+" Age = "+age+" Percentage= "+percentage);
+		
+	}
+
+}
